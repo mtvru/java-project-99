db4da726e0f11e624887103a6057cdc79ae0b69c
maksimus <maksimtsoi.nsk@gmail.com>
2026-04-20 22:19:13 +0600

    chore

--- src/main/java/hexlet/code/app/controller/WelcomeController.java
+++ src/main/java/hexlet/code/app/controller/WelcomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
     @GetMapping("/welcome")
-    public String home() {
+    public String index() {
         return "Welcome to Spring";
     }
 }
