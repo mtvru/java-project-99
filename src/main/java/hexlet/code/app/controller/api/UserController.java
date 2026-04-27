25c8e29012a99eff2a459ad4f6ee7fb37adbf377
maksimus <maksimtsoi.nsk@gmail.com>
2026-04-27 15:22:26 +0600

    done todo

--- src/main/java/hexlet/code/app/controller/api/UserController.java
+++ src/main/java/hexlet/code/app/controller/api/UserController.java
@@ -2,20 +2,19 @@ package hexlet.code.app.controller.api;
 
 import hexlet.code.app.dto.UserCreateDTO;
 import hexlet.code.app.dto.UserDTO;
-import hexlet.code.app.dto.UserPatchDTO;
 import hexlet.code.app.dto.UserUpdateDTO;
-import hexlet.code.app.exception.ResourceNotFoundException;
-import hexlet.code.app.mapper.UserMapper;
-import hexlet.code.app.model.User;
-import hexlet.code.app.repository.UserRepository;
-import jakarta.validation.Valid;
+import hexlet.code.app.service.UserService;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -23,66 +22,45 @@ import java.net.URI;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    private final UserRepository userRepository;
-    private final UserMapper userMapper;
+    private final UserService userService;
 
-    public UserController(UserRepository userRepository, UserMapper userMapper) {
-        this.userRepository = userRepository;
-        this.userMapper = userMapper;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping
     public ResponseEntity<Page<UserDTO>> index(@RequestParam(defaultValue = "10") Integer limit) {
-        Pageable pageable = PageRequest.of(0, limit, Sort.by("createdAt").descending());
-        Page<User> users = this.userRepository.findAll(pageable);
-        return ResponseEntity.ok(users.map(this.userMapper::map));
+        Page<UserDTO> users = this.userService.findAll(0, limit);
+        return ResponseEntity.ok(users);
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public ResponseEntity<UserDTO> create(@Valid @RequestBody UserCreateDTO dto) {
-        User user = this.userMapper.map(dto);
-        user = this.userRepository.save(user);
+    public ResponseEntity<UserDTO> create(@RequestBody UserCreateDTO dto) {
+        UserDTO user = this.userService.create(dto);
         URI location = ServletUriComponentsBuilder
             .fromCurrentRequest()
             .path("/{id}")
             .buildAndExpand(user.getId())
             .toUri();
-        return ResponseEntity.created(location)         //higher priority than @ResponseStatus(HttpStatus.CREATED)
-            .body(this.userMapper.map(user));
+        return ResponseEntity.created(location)
+            .body(user);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> show(@PathVariable Long id) {
-        User user = this.userRepository.findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException("User with id " + id + " not found"));
-        return ResponseEntity.ok(this.userMapper.map(user));
+        UserDTO user = this.userService.findById(id);
+        return ResponseEntity.ok(user);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO dto) {
-        User user = this.userRepository.findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException("User with id " + id + " not found"));
-        this.userMapper.update(dto, user);
-        this.userRepository.save(user);
-        return ResponseEntity.ok(this.userMapper.map(user));
-    }
-
-    @PatchMapping("/{id}")
-    public ResponseEntity<UserDTO> patch(@PathVariable Long id, @Valid @RequestBody UserPatchDTO dto) {
-        var user = userRepository.findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException("User with id " + id + " not found"));
-        this.userMapper.update(dto, user);
-        userRepository.save(user);
-        return ResponseEntity.ok(this.userMapper.map(user));
+    public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody UserUpdateDTO dto) {
+        UserDTO user = this.userService.update(id, dto);
+        return ResponseEntity.ok(user);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> destroy(@PathVariable Long id) {
-        if (!this.userRepository.existsById(id)) {
-            return ResponseEntity.notFound().build();
-        }
-        this.userRepository.deleteById(id);
+        this.userService.delete(id);
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/hexlet/code/app/service/UserService.java
+++ src/main/java/hexlet/code/app/service/UserService.java
@@ -1,18 +1,62 @@
 package hexlet.code.app.service;
 
+import hexlet.code.app.dto.UserCreateDTO;
+import hexlet.code.app.dto.UserDTO;
+import hexlet.code.app.dto.UserUpdateDTO;
+import hexlet.code.app.exception.ResourceNotFoundException;
+import hexlet.code.app.mapper.UserMapper;
+import hexlet.code.app.model.User;
 import hexlet.code.app.repository.UserRepository;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 @Service
+@Validated
 public class UserService {
-    private final UserRepository userRepository;
+    private final UserRepository repository;
+    private final UserMapper mapper;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
+        this.repository = userRepository;
+        this.mapper = userMapper;
     }
 
-    //TODO
-    public void createUser() {
-        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+    public UserDTO create(@Valid UserCreateDTO dto) {
+        User user = this.mapper.map(dto);
+        user = this.repository.save(user);
+        return this.mapper.map(user);
+    }
+
+    public Page<UserDTO> findAll(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
+        Page<User> users = this.repository.findAll(pageable);
+        return users.map(this.mapper::map);
+    }
+
+    public UserDTO findById(Long id) {
+        return this.repository.findById(id)
+                .map(this.mapper::map)
+                .orElseThrow(() -> new ResourceNotFoundException("User with id " + id + " not found"));
+    }
+
+    public UserDTO update(Long id, @Valid UserUpdateDTO dto) {
+        User user = this.repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User with id " + id + " not found"));
+        this.mapper.update(dto, user);
+        this.repository.save(user);
+        return this.mapper.map(user);
+    }
+
+    public boolean delete(Long id) {
+        if (!this.repository.existsById(id)) {
+            return false;
+        }
+        this.repository.deleteById(id);
+        return true;
     }
 }
